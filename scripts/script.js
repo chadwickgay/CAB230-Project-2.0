1a5bf99b9afb5caa02c1697fdc9098e0738ab441
Unknown <chadwick.gay@connect.qut.edu.au>
2017-04-18 12:07:45 +1000

    Updated checkDOB / checkFirstName/ checkLastName
    Names - now allow for spaces, hypens, periods in single function
    DOB - validates filed not empty, matches valid format (inc leap years), within plausible range. All done in single function now

--- scripts/script.js
+++ scripts/script.js
@@ -9,49 +9,12 @@ function validate() {
 
 }
 
-// These functions aren't needed if we just use an update Regex query (see below)
-
-function isNumeric(textToValidate) {
-	return /^\d+$/.test(textToValidate);
-}
-
-function isAlphabeticOnly(textToValidate) {
-	return /^[a-zA-Z\-]+$/.test(textToValidate);
-}
-
-//returns the number of valid words in string.
-//returns the index (starts from 1) of first invalid word in sentence if any.
-function isNumOfWords(textToValidate) {
-	var words = textToValidate.split(' ');
-	if (textToValidate.length < 1 || words.length < 1) {
-		return 0;
-	}
-	for (var i = 0; i < words.length; i++) {
-		if (!isAlphabeticOnly(words[i])) {
-			return -1 * (i+1);
-		}
-	}
-	return words.length;
-} 
-
-// end uneeded functions
-
-
-// I have updated the checkFirstName so you can see it working / test it.
-// Seems simpler to use 1 func, rather than 4 to achieve same thing
 
 function checkFirstName() {
-    // updated Regex query to allow for spaces/hyphen/period as part of name 
-    validNameFormat = /^[a-zA-Z \,\.\'\-]+$/; 
-    
-    /* valid examples
-    
-    Mathias d'Arras
-    Martin Luther King, Jr.
-    Hector Sausage-Hausen
-    
-    */
-    
+    // Format for valid name
+    // Allows spaces/hyphen/period as part of name
+    validNameFormat = /^[a-zA-Z \,\.\'\-]+$/;
+
     //Store the name field objects into variables ...
     var name = document.forms["myForm"]["first-name"];
     //Store the Confimation Message Object ...                
@@ -62,7 +25,7 @@ function checkFirstName() {
         message.style.color = errorColour;
         message.innerHTML = "You must enter a first name!";
         return false;
-	} else if (!validNameFormat.test(name.value)) {
+    } else if (!validNameFormat.test(name.value)) {
         name.style.backgroundColor = errorColour;
         message.style.color = errorColour;
         message.innerHTML = "Enter your first name just using letters!";
@@ -73,6 +36,9 @@ function checkFirstName() {
 }
 
 function checkLastName() {
+    // Format for valid name
+    // Allows spaces/hyphen/period as part of name
+    validNameFormat = /^[a-zA-Z \,\.\'\-]+$/;
 
     //Store the name field objects into variables ...
     var name = document.forms["myForm"]["last-name"];
@@ -84,7 +50,7 @@ function checkLastName() {
         message.style.color = errorColour;
         message.innerHTML = "You must enter a last name!";
         return false;
-	} else if (isNumOfWords(name.value) <= 0) {
+    } else if (!validNameFormat.test(name.value)) {
         name.style.backgroundColor = errorColour;
         message.style.color = errorColour;
         message.innerHTML = "Enter your last name just using letters!";
@@ -95,20 +61,23 @@ function checkLastName() {
 }
 
 function resetErrorState(htmlElementName, confirmMessageID) {
-    //Store the name field objects into variables ...
-    var name = document.forms["myForm"][htmlElementName];
+    //Store the element name into variable 
+    var elementName = document.forms["myForm"][htmlElementName];
     //Store the Confimation Message Object ...                
     var message = document.getElementById(confirmMessageID);
 
-    name.style.backgroundColor = noErrorColor;
+    elementName.style.backgroundColor = noErrorColor;
     message.style.color = noErrorColor;
     message.innerHTML = "";
 }
 
 function checkEmail() {
-    // Letter format for name
-    var validEmail = /^[a-z][a-zA-Z0-9_.]*(\.[a-zA-Z][a-zA-Z0-9_.]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$/;
-    //Store the name field objects into variables ...
+    // Format for valid email address
+    // Allows letters, numbers, hypens, underscores and periods in address
+    // Must have word followed by @ domain name . com
+    // Valid example - email@example.com
+    var validEmail = /^[a-z][a-zA-Z0-9_.-]*(\.[a-zA-Z][a-zA-Z0-9_.-]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$/;
+    //Store the email field objects into variables ...
     var email = document.forms["myForm"]["email"];
     //Store the Confimation Message Object ...                
     var message = document.getElementById('emailConfirmMessage');
@@ -128,39 +97,24 @@ function checkEmail() {
     }
 }
 
-//valid input: d[d]/m[m]/yyyy slashes(/) or dots(.) work.
-//must be full year as anything less can be confusing.
-//returns the year of birth if valid, else false.
-function isValidDate(textToValidate) {
-	var parts = textToValidate.split('/');
-	if (parts.length != 3) {
-		parts = textToValidate.split('.');
-	}
-	if (parts.length != 3) {
-		return false;
-	}
-	if (parts[2].length != 4 || !isNumeric(parts[2])) {
-		return false;
-	}
-	//months indexing begins at 0.
-	//if numbers are negative it goes previous year/month etc.
-	//so just a month checks works.
-	var date = new Date(parts[2], parts[1] - 1, parts[0]);
-	if (date && (date.getMonth() + 1) == parts[1]) {
-		return date.getFullYear();
-	}
-	return false;
-}
-
 function checkDOB() {
-    //Store the DOB field objects into variables ...
+    // Format for valid DOB
+    // Checks basic format - e.g 01/01/2010 is valid. 
+    // Checks number of days in month - e.g. 44/05/2017 is invalid.
+    // Checks number of days in month for leaps years.
+    validDobFormat = /^(?:(?:31(\/)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
+
+    // store the DOB field objects into variables
     var dob = document.forms["myForm"]["dob"];
     // oldest age allowed as valid DOB
-    var oldestAge = 120;
+    var oldestAge = 123; // oldest age ever recorded
     // current year
     var currentYear = new Date().getFullYear();
     //Store the Confimation Message Object ...                
     var message = document.getElementById('dobConfirmMessage');
+    //Split date into parts and get the year entered
+    var parts = dob.value.split('/');
+    var yearEntered = parts[2];
 
     // If DOB field empty
     if (dob.value == "") {
@@ -168,28 +122,28 @@ function checkDOB() {
         message.style.color = errorColour;
         message.innerHTML = "You must enter a date of birth!";
         return false;
-    }
-    
-    // Check for valid DOB
-	var validDob = isValidDate(dob.value);
-	
-    if (validDob == false) {
-		dob.style.backgroundColor = errorColour;
+
+    } // If DOB does not match valid format
+    else if (!validDobFormat.test(dob.value)) {
+        dob.style.backgroundColor = errorColour;
         message.style.color = errorColour;
-        message.innerHTML = "Date of birth needs to be in the format: dd/mm/yyyy";
+        message.innerHTML = "Date of birth needs to be valid date in the format: dd/mm/yyyy!";
         return false;
-	} else if (validDob > (currentYear - oldestAge) && validDob <= currentYear) {
-        return true;
+
+    } // If DOB is not within plausible/reasnable range 
+    else if (yearEntered < (currentYear - oldestAge || yearEntered > currentYear)) {
+        dob.style.backgroundColor = errorColour;
+        message.style.color = errorColour;
+        message.innerHTML = "Age outside allowed range: " + (currentYear - oldestAge) + " to " +
+            currentYear + "!";
+        return false;
     } else {
-		dob.style.backgroundColor = errorColour;
-        message.style.color = errorColour;
-        message.innerHTML = "You must enter a valid date of birth!";
-        return false;
+        return true;
     }
 }
 
 function checkGender() {
-    //Store the state field objects into variables ...
+    //Store the gender field objects into variables ...
     var gender = document.forms["myForm"]["gender"];
     //Store the Confimation Message Object ...                
     var message = document.getElementById('genderConfirmMessage');
@@ -198,7 +152,9 @@ function checkGender() {
         message.innerHTML = "You must select a gender!";
         message.style.color = errorColour;
         return false;
-    } else return true;
+    } else {
+        return true;
+    }
 
 }
 
