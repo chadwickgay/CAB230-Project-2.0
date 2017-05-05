2845d8654e922f6720e4d0f339569e1eb9b160c9
Unknown <chadwick.gay@connect.qut.edu.au>
2017-05-05 16:11:04 +1000

    Worth with multiple forms

--- scripts/script.js
+++ scripts/script.js
@@ -5,20 +5,21 @@ var noErrorColor = "#ffffff";
 
 function validate() {
 
-    return checkEmail() & checkGender() & checkFirstName() & checkLastName() & checkPassword() & checkDOB();
+    return checkEmail("createAccount") & checkGender("createAccount") & checkFirstName("createAccount")
+        & checkLastName("createAccount") & checkPassword() & checkDOB("createAccount");
 
 }
 
 
-function checkFirstName() {
+function checkFirstName(formName) {
     // Format for valid name
     // Allows spaces/hyphen/period as part of name
-    validNameFormat = /^[a-zA-Z \,\.\'\-]+$/;
+    validNameFormat = /^[a-zA-Z ,.'\-]+$/;
 
     //Store the name field objects into variables ...
-    var name = document.forms["myForm"]["first-name"];
-    //Store the Confimation Message Object ...                
-    var message = document.getElementById('first-nameConfirmMessage');
+    var name = document.forms[formName]["first-name"];
+    //Store the Error Message Object ...                
+    var message = document.getElementById('first-nameErr');
 
     if (name.value == "") {
         name.style.backgroundColor = errorColour;
@@ -35,15 +36,15 @@ function checkFirstName() {
     }
 }
 
-function checkLastName() {
+function checkLastName(formName) {
     // Format for valid name
     // Allows spaces/hyphen/period as part of name
     validNameFormat = /^[a-zA-Z \,\.\'\-]+$/;
 
     //Store the name field objects into variables ...
-    var name = document.forms["myForm"]["last-name"];
-    //Store the Confimation Message Object ...                
-    var message = document.getElementById('last-nameConfirmMessage');
+    var name = document.forms[formName]["last-name"];
+    //Store the Error Message Object ...                
+    var message = document.getElementById('last-nameErr');
 
     if (name.value == "") {
         name.style.backgroundColor = errorColour;
@@ -60,27 +61,27 @@ function checkLastName() {
     }
 }
 
-function resetErrorState(htmlElementName, confirmMessageID) {
+function resetErrorState(htmlElementName, ErrID, formName) {
     //Store the element name into variable 
-    var elementName = document.forms["myForm"][htmlElementName];
-    //Store the Confimation Message Object ...                
-    var message = document.getElementById(confirmMessageID);
+    var elementName = document.forms[formName][htmlElementName];
+    //Store the Error Message Object ...                
+    var message = document.getElementById(ErrID);
 
     elementName.style.backgroundColor = noErrorColor;
     message.style.color = noErrorColor;
     message.innerHTML = "";
 }
 
-function checkEmail() {
+function checkEmail(formName) {
     // Format for valid email address
     // Allows letters, numbers, hypens, underscores and periods in address
     // Must have word followed by @ domain name . com
     // Valid example - email@example.com
     var validEmail = /^[a-z][a-zA-Z0-9_.-]*(\.[a-zA-Z][a-zA-Z0-9_.-]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$/;
     //Store the email field objects into variables ...
-    var email = document.forms["myForm"]["email"];
-    //Store the Confimation Message Object ...                
-    var message = document.getElementById('emailConfirmMessage');
+    var email = document.forms[formName]["email"];
+    //Store the Error Message Object ...                
+    var message = document.getElementById('emailErr');
 
     if (email.value == "") {
         email.style.backgroundColor = errorColour;
@@ -97,7 +98,7 @@ function checkEmail() {
     }
 }
 
-function checkDOB() {
+function checkDOB(formName) {
     // Format for valid DOB
     // Checks basic format - e.g 01/01/2010 is valid. 
     // Checks number of days in month - e.g. 44/05/2017 is invalid.
@@ -105,13 +106,13 @@ function checkDOB() {
     validDobFormat = /^(?:(?:31(\/)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
 
     // store the DOB field objects into variables
-    var dob = document.forms["myForm"]["dob"];
+    var dob = document.forms[formName]["dob"];
     // oldest age allowed as valid DOB
     var oldestAge = 123; // oldest age ever recorded
     // current year
     var currentYear = new Date().getFullYear();
-    // Store the Confimation Message Object ...                
-    var message = document.getElementById('dobConfirmMessage');
+    // Store the Error Message Object ...                
+    var message = document.getElementById('dobErr');
     // Split date into parts 
     var parts = dob.value.split('/');
     // Retrieve the year entered
@@ -143,11 +144,11 @@ function checkDOB() {
     }
 }
 
-function checkGender() {
+function checkGender(formName) {
     //Store the gender field objects into variables ...
-    var gender = document.forms["myForm"]["gender"];
-    //Store the Confimation Message Object ...                
-    var message = document.getElementById('genderConfirmMessage');
+    var gender = document.forms[formName]["gender"];
+    //Store the Error Message Object ...                
+    var message = document.getElementById('genderErr');
 
     if (gender.value == "DEFAULT") {
         message.innerHTML = "You must select a gender!";
@@ -163,8 +164,8 @@ function checkPassword() {
     //Store the password field objects into variables ...
     var pass1 = document.getElementById('password1');
     var pass2 = document.getElementById('password2');    
-    //Store the Confimation Message Object ...                
-    var message = document.getElementById('passwordConfirmMessage');
+    //Store the Error Message Object ...                
+    var message = document.getElementById('passwordErr');
 
     //Compare the values in the password field 
     //and the confirmation field
