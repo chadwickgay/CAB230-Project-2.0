a001d55a128af42040b2df9da7996df7038b1eb9
Unknown <chadwick.gay@connect.qut.edu.au>
2017-05-05 16:12:18 +1000

    Removed html form from page

--- create-account.php
+++ create-account.php
@@ -30,90 +30,34 @@
 
 <body>
 
-    <div class="container">
+<div class="container">
 
-        <!-- Page Navigation
-    –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <!-- Page Navigation
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
 
-        <?php include('includes/nav.inc'); ?>
+    <?php include('includes/nav.inc'); ?>
 
-        <!-- Main Content
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <!-- Main Content
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
 
-        <h3>Create an account</h3>
+    <h3>Create an account</h3>
 
-        <form name="myForm" method="get">
-            <div class="row">
-                <div class="two columns">
-                    <label for="first-name">First Name</label>
-                    <input type="text" placeholder="John" name="first-name" id="first-name" onkeypress="resetErrorState('first-name', 'first-nameConfirmMessage')" />
-                    <br>
-                    <span id="first-nameConfirmMessage"></span>
+    <?php include('includes/createAccountForm.inc'); ?>
 
-
-                    <label for="last-name">Last Name</label>
-                    <input type="text" placeholder="Citizen" name="last-name" id="last-name" onkeypress="resetErrorState('last-name', 'last-nameConfirmMessage')" />
-                    <br>
-                    <span id="last-nameConfirmMessage"></span>
-
-                    <label for="dob">Date of Birth (dd/mm/yyyy)</label>
-                    <input type="text" name="dob" id="dob" placeholder="dd/mm/yyyy" onkeypress="resetErrorState('dob', 'dobConfirmMessage')"/>
-                    <br>
-                    <span id="dobConfirmMessage"></span>
-
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" placeholder="email@example.com" onkeypress="resetErrorState('email', 'emailConfirmMessage')" />
-                    <br>
-                    <span id="emailConfirmMessage"></span>
-
-                </div>
-
-                <div class="two columns">
-                    <label for="gender">Gender</label>
-                    <select class="gender-select" name="gender" id="gender">
-						<option value="DEFAULT" disabled selected style="display: none;">select</option>
-						<option value="m">Male</option>
-						<option value="f">Female</option>
-						<option value="o">Other</option>
-						<option value="u">Undisclosed</option>
-					</select>
-                    <br>
-                    <span id="genderConfirmMessage"></span>
-
-                    <label for="password1">Password</label>
-                    <input type="password" name="password" id="password1" placeholder="password" onkeypress="resetErrorState('password', 'passwordConfirmMessage')" />
-                    <br>
-
-                    <input type="password" name="confirm-password" id="password2" placeholder="confirm password" onkeypress="resetErrorState('confirm-password', 'passwordConfirmMessage')" />
-                    <br>
-                    <span id="passwordConfirmMessage"></span>
-
-                    <br>
-                    <input type="checkbox" name="subscribe" value="subscribe">Subscribe to the weekly Park Search newsletter to receive
-                    <br>the latest updates about your favourite parks.
-                </div>
-            </div>
-
-            <div class="one column">
-                <input type="button" onclick="validate()" value="Submit">
-                <p class="message">Already registered? <a href="login.html">Sign In</a></p>
-            </div>
-
-
-        </form>
-
-
-        <!-- Footer
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-
-        <footer class="one column">
-            <p>Copyright &copy; Chadwick Gay | Tom Deakin | Kenneth Koefler</p>
-        </footer>
-
-
-        <!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+    <div class="one column">
+        <input type="button" onclick="validate()" value="Submit">
+        <p class="message">Already registered? <a href="login.html">Sign In</a></p>
     </div>
+
+    <!-- Footer
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <?php include('includes/footer.inc'); ?>
+
+
+    <!-- End Document
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</div>
 
 
 </body>
