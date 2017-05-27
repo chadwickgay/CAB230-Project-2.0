5f44e3cec956ad9749df3896b5aef643c782f987
Unknown <chadwick.gay@connect.qut.edu.au>
2017-05-27 14:42:55 +1000

    Fixed incorrect validation error message and appropriate symbols

--- scripts/validation.js
+++ scripts/validation.js
@@ -57,10 +57,10 @@ function validateFirstName(formName) {
     // Format for valid name
     // Allows spaces/hyphen/period as part of name
     var validNameFormat = /^[a-zA-Z ,.'\-]+$/;
-    
+
     //Store the name field objects into variables ...
     var name = document.forms[formName]["first-name"];
-    
+
     //Store the Error Message Object ...                
     var message = document.getElementById('first-nameErr');
 
@@ -88,7 +88,7 @@ function validateLastName(formName) {
 
     //Store the name field objects into variables ...
     var name = document.forms[formName]["last-name"];
-    
+
     //Store the Error Message Object ...                
     var message = document.getElementById('last-nameErr');
 
@@ -114,11 +114,11 @@ function validateEmail(formName) {
     // Allows letters, numbers, hypens, underscores and periods in address
     // Must have word followed by @ domain name . com
     // Valid example - email@example.com
-    
+
     var validEmail = /^[a-z][a-zA-Z0-9_.-]*(\.[a-zA-Z][a-zA-Z0-9_.-]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$/;
     //Store the email field objects into variables ...
     var email = document.forms[formName]["email"];
-    
+
     //Store the Error Message Object ...                
     var message = document.getElementById('emailErr');
 
@@ -137,26 +137,26 @@ function validateEmail(formName) {
 
 function validateDOB(formName) {
     // Format for valid DOB
-    // Checks basic format - e.g 01/01/2010 is valid. 
+    // Checks basic format - e.g 01/01/2010 is valid.
     // Checks number of days in month - e.g. 44/05/2017 is invalid.
     // Checks number of days in month for leaps years.
     validDobFormat = /^(?:(?:31(\/)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/
 
     // store the DOB field objects into variables
     var dob = document.forms[formName]["dob"];
-    
+
     // oldest age allowed as valid DOB
     var oldestAge = 123; // oldest age ever recorded
-    
+
     // current year
     var currentYear = new Date().getFullYear();
-    
+
     // Store the Error Message Object ...                
     var message = document.getElementById('dobErr');
-    
-    // Split date into parts 
+
+    // Split date into parts
     var parts = dob.value.split('/');
-    
+
     // Retrieve the year entered
     var yearEntered = parts[2];
 
@@ -172,7 +172,7 @@ function validateDOB(formName) {
         message.innerHTML = "Date of birth needs to be valid date in the format: dd/mm/yyyy!";
         return false;
 
-    } // If DOB is not within plausible/reasnable range 
+    } // If DOB is not within plausible/reasnable range
     else if (yearEntered < (currentYear - oldestAge || yearEntered > currentYear)) {
         setErrorState(dob, message);
         message.innerHTML = "Age outside allowed range: " + (currentYear - oldestAge) + " to " +
@@ -186,7 +186,7 @@ function validateDOB(formName) {
 function validateGender(formName) {
     //Store the gender field objects into variables ...
     var gender = document.forms[formName]["gender"];
-    
+
     //Store the Error Message Object ...                
     var message = document.getElementById('genderErr');
 
@@ -204,11 +204,11 @@ function validatePassword() {
     //Store the password field objects into variables ...
     var pass1 = document.getElementById('password1');
     var pass2 = document.getElementById('password2');
-    
+
     //Store the Error Message Object ...                
     var message = document.getElementById('passwordErr');
 
-    //Compare the values in the password field 
+    //Compare the values in the password field
     //and the confirmation field
     if (pass1.value == "" || pass2.value == "") {
 		if (pass1.value == "")
@@ -219,9 +219,9 @@ function validatePassword() {
         message.innerHTML = "You must enter a password!";
         return false;
     } else if (pass1.value == pass2.value) {
-        //The passwords match. 
+        //The passwords match.
         //Set the color to the good color and inform
-        //the user that they have entered the correct password 
+        //the user that they have entered the correct password
         pass1.style.backgroundColor = successColour;
         pass2.style.backgroundColor = successColour;
         message.style.color = successColour;
@@ -248,7 +248,7 @@ function validateComment(formName) {
 
     //Store the name field objects into variables ...
     var comment = document.forms[formName]["txtcomment"];
-    
+
     //Store the Error Message Object ...                
     var message = document.getElementById('txtcommentErr');
 
@@ -258,7 +258,7 @@ function validateComment(formName) {
         return false;
     } else if (!validCommentFormat.test(comment.value)) {
         setErrorState(comment, message);
-        message.innerHTML = "Enter your last name just using letters!";
+        message.innerHTML = "Your comments made only use letters, numbers, hypens and periods!";
         return false;
     } else if (comment.value.length < 2 || comment.value.length > 256) {
         setErrorState(comment, message);
@@ -278,7 +278,7 @@ function setErrorState(inputName, errorField) {
 function resetErrorState(htmlElementName, ErrID, formName) {
     //Store the element name into variable
     var elementName = document.forms[formName][htmlElementName];
-    
+
     //Store the Error Message Object ...                
     var message = document.getElementById(ErrID);
 
