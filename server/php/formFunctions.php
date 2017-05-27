caf3552ea3dee10d332ec900a147f3a1761dbc87
Incurable <t.deakin@live.com.au>
2017-05-28 04:19:14 +1000

    Removed old function.

--- server/php/formFunctions.php
+++ server/php/formFunctions.php
@@ -44,15 +44,4 @@
 		echo "</select>";
 	}
 
-	function createSelectField_OLD($name, $values) {
-		echo "<select id=\"$name\" name=\"$name\">";
-		
-		foreach ($values as $value => $display) {
-			$selected = ($value==posted_value($name))?'selected="selected"':'';
-			echo "<option $selected value=\"$value\">$display</option>";
-		}
-		
-		echo '</select>';
-	}
-
 ?>
