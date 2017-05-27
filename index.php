faab273f9a27ff7ed96a84e762579ac397605636
Unknown <chadwick.gay@connect.qut.edu.au>
2017-05-27 21:16:49 +1000

    Title for hero image

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     <!-- Main Content
 –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 
-    <div id="hero-image">
+    <div id="hero-image" title="Man sitting in a park beside a path reading a book.">
         <div>
             <h2><strong>A simple, easy</strong><br/> way to find parks near you.</h2>
             <a href="browse.php" class="button-hero">Get Started</a>
