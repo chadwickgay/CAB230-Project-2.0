c655dd94c9a5e7b0a68cd04974bc08079ff90066
Incurable <t.deakin@live.com.au>
2017-05-28 03:13:35 +1000

    Created functions for repeated code in accountForm.inc

--- server/php/formFunctions.php
+++ server/php/formFunctions.php
@@ -1,36 +1,50 @@
 <?php
 
 	// Create a label tag for the name of a form
-	function createLabel($name, $label) {
-		echo "<label for='$name'>$label</label>";
+	function createLabel($for, $labelName) {
+		echo "<label for='$for'>$labelName</label>";
 	}
 
 	// Create an error tag using the HTML span tag
-	function createErrorLabel($name) {
-		echo "<span for='$name'</span>";
+	function createErrorLabel($for) {
+		echo "<span for='$for'</span>";
 	}
 
 	// Create and assign the value attribute with the appropriate POST name
 	function createPostedValue($name) {
-		if(isset($_POST[$name])) {
-			echo "value='htmlspecialchars($_POST[$name])'";
+		if (isset($_POST["$name"])) {
+			return "".htmlspecialchars($_POST[$name]);
 		} else {
-			echo "value=''";
+			return "";
 		}
 	}
 	
-	function createInputField($type, $name, $placeholder, $error, $formName) {
-		createLabel($name, $placeholder);
+	// Create an input field with appropriate attributes for a HTML5 form
+	function createInputField($type, $id, $name, $labelName, $placeholder, $error, $formName) {
+		createLabel($placeholder, $labelName);
 		$value = createPostedValue($name);
-		echo "<input type='$type' id='$name' name='$name' ",
+		echo "<input type='$type' id='$id' name='$name' ",
 			 "placeholder='$placeholder' value='$value' ",
-			 "onKeyPress=\"$name\", \"$error\", \"$formName\" />";
+			 "onKeyPress='$name', '$error', '$formName' />";
 		echo "<br>";
 		createErrorLabel($name);
 	}
 
-	// Create a form option list with the select tag that includes posted values
-	function createSelectField($name, $values) {
+	// Create an option list with the select tag and appropriate attributes
+	function createSelectField($for, $class, $id, $labelName, $options, $values, $default, $error) {
+		createLabel($for, $labelName);
+		echo "<select class='$class', id='$id', name='$for'>";
+		echo "<option selected value='DEFAULT' disabled style='display: none;'>$default</option>";		
+		
+		define('MAX_OPTIONS', 4);
+		for($optionIndex = 0; $optionIndex <= MAX_OPTIONS; $optionIndex++) {
+			echo '<option value="'.$values[$optionIndex].'">'.$options[$optionIndex].'</option>';
+		}
+		
+		echo "</select>";
+	}
+
+	function createSelectField_OLD($name, $values) {
 		echo "<select id=\"$name\" name=\"$name\">";
 		
 		foreach ($values as $value => $display) {
