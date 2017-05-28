ecf73691bb913df6060c0585695be8acc0ae19ea
Unknown <chadwick.gay@connect.qut.edu.au>
2017-05-28 23:33:08 +1000

    Completed create account inputs via php

--- server/php/formFunctions.php
+++ server/php/formFunctions.php
@@ -29,40 +29,13 @@
 		createErrorLabel($errorMessageBox);
 	}
 
-	// Create an option list with the select tag and appropriate attributes
-/*
-function createSelectField($for, $class, $id, $labelName, $options, $values, $default, $error) {
-		createLabel($for, $labelName);
-		echo "<select class='$class', id='$id', name='$for'>";
-		echo "<option selected value='DEFAULT' disabled style='display: none;'>$default</option>";		
-		
-		define('MAX_OPTIONS', 4);
-		for($optionIndex = 0; $optionIndex <= MAX_OPTIONS; $optionIndex++) {
-			echo '<option value="'.$values[$optionIndex].'">'.$options[$optionIndex].'</option>';
-		}
-		
-		echo "</select>";
-	}
-*/
-
-/*
-            <label for="gender">Gender</label>
-            <select class="gender-select" name="gender" id="gender">
-                <option selected value="DEFAULT" disabled style="display: none;">select</option>
-                <option <?php echo posted_value('gender') == 1 ? 'selected' : '' ?> value="1">Male</option>
-                <option <?php echo posted_value('gender') == 2 ? 'selected' : '' ?> value="2">Female</option>
-                <option <?php echo posted_value('gender') == 3 ? 'selected' : '' ?> value="3">Other</option>
-                <option <?php echo posted_value('gender') === 0 ? 'selected' : '' ?> value="0">Undisclosed</option>
-            </select>
-            <br>
-            <span id="genderErr"></span>
-*/
-
+// Create an option list with the select tag and appropriate attributes
 function createSelectField($id, $labelName, $name, $errorSpanID, $options, $formName) {
     createLabel($id, $labelName);
 
-    echo "<select id='$id'name='$name' onkeypress='resetErrorState('$name', '$errorSpanID', '$formName')'>";
-        echo "<option selected value='DEFAULT' disabled class=\"hidden\">select</option>";
+    echo "<select id=\"$id\" name=\"$name\" onChange=\"resetErrorState('$name', '$errorSpanID', '$formName')\">";
+
+    echo "<option selected value='DEFAULT' disabled class=\"hidden\">select</option>";
 
         for ($i = 0; $i < count($options); $i++){
             echo "<option value='$i'>$options[$i]</option>";
